ae522d708b780c9c3f93ff3fed62b11a04f85f73
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-11 13:01:03 -0600

    atomic basic components 2

---
+++ login_app/lib/ui/widgets/submit_button.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SubmitVal extends StatelessWidget {
+ @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text("Send"),
+      onPressed: (){},
+    );
+ }
+}
