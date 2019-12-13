b63c1eaf2acfc28cd1ea2f96b598779dff08841f
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-13 16:22:28 -0600

    widget

--- msg_firebase/lib/ui/screens/welcome_screen.dart
+++ msg_firebase/lib/ui/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:msg_firebase/ui/widgets/button_widget.dart';
 
 class Welcome extends StatefulWidget {
   @override
@@ -9,14 +10,32 @@ class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: 25.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment:  CrossAxisAlignment.stretch,
         children: <Widget>[
           Row(
             children: <Widget>[
-              Text("hola"),
-              Text("Firebase")
+            Image.asset('images/icons8.png'),
+            Text(
+              " Flutter-APP",
+              style: TextStyle(
+                fontSize: 45.0,
+                fontWeight: FontWeight.w700
+              ),
+            )
             ],
+          ),
+          ButtonWgt(
+            Colors.lightBlueAccent,
+            "Login",
+            (){}
+          ),
+          ButtonWgt(
+            Colors.blueAccent,
+            "Register",
+            (){}
           )
         ],
       ),
