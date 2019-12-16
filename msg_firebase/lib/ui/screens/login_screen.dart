a14feb0798dfa49e1abf59b60f03620ec8beb1c6
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-15 21:17:37 -0600

    adding styles to textfield widget

--- msg_firebase/lib/ui/screens/login_screen.dart
+++ msg_firebase/lib/ui/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:msg_firebase/ui/widgets/button_widget.dart';
+import 'package:msg_firebase/ui/widgets/icon_widget.dart';
+import 'package:msg_firebase/ui/widgets/text_widget.dart';
 
 class Login extends StatefulWidget {
   static const String routeName = "/login";
@@ -8,9 +11,27 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-    appBar:  AppBar(
-      title: Text("holas") ,),
-   );
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal:  24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            IconWgt(),
+            SizedBox( height: 48.0),
+            TextWgt("Write email"),
+            SizedBox( height: 8.0),
+            TextWgt("Write password"),
+            SizedBox( height: 23.0),
+            ButtonWgt(
+              Colors.blueAccent,
+              "Log In",
+              (){}
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
