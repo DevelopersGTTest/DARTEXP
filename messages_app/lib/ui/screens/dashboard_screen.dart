3793f5e5112365b801dd37636d98c4980f43d968
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-18 15:54:33 -0600

    log a simple data

--- messages_app/lib/ui/screens/dashboard_screen.dart
+++ messages_app/lib/ui/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Dashboard extends StatefulWidget {
   static const String routeName = "/dashboard";
@@ -6,6 +7,29 @@ class Dashboard extends StatefulWidget {
   _DashboardState createState() => new _DashboardState();
  }
 class _DashboardState extends State<Dashboard> {
+  final auth = FirebaseAuth.instance;
+  FirebaseUser loogerUser;
+
+  //is onInit in Flutter
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.getCurrentUser();
+  }
+
+  //temp method
+  void getCurrentUser() async {
+    try {
+      var user = await auth.currentUser();
+      if( user != null ){
+        this.loogerUser = user;
+        print("[Current user id] ::: $user ");
+      }
+    } catch (e) {
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
