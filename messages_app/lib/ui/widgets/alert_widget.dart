56e8842583cdc102044a9645969fdac3b223e14d
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-21 22:36:59 -0600

    adding content alert dialog

--- messages_app/lib/ui/widgets/alert_widget.dart
+++ messages_app/lib/ui/widgets/alert_widget.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 class AlertWgt extends StatelessWidget {
+  //properties
+  String data = "";
+  //String typeAlert = ""; not use
+
+  AlertWgt({ data :  })
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text(
         "Upss....",
-        style:  TextStyle( color: Colors.redAccent) ), //is use exceptions
+        style:  TextStyle( 
+          color: Colors.redAccent
+        )
+      ),
+      content: , //is use exceptions
     );
  }
 }
