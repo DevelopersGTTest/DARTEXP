041fe1865dca47ce9ac630fb36f6f90f70f68b3a
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-23 12:32:54 -0600

    part 1 simple validate auth

--- messages_app/lib/services/auth_service.dart
+++ messages_app/lib/services/auth_service.dart
@@ -1,20 +1,26 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:messages_app/models/authentication_request.dart';
 
 class Auth {
   final _auth = FirebaseAuth.instance;
 
   //register method
-  Future<FirebaseUser> createUser(
+  Future<AuthenticationRequest> createUser(
   { String emailParam = "", String passwordParam = "" }) async {
+    AuthenticationRequest authRequest = new AuthenticationRequest();
     try {
-      return await this._auth
+      var user = await this._auth
         .createUserWithEmailAndPassword( 
           email: emailParam, password: passwordParam
         );
+      if( user != null ){
+        authRequest.success = true;
+      }
     } catch (e) {
       print("[Error in register user] ::: $e ");
+      this._mapError(authRequest, e.code);
     }
-    return null;
+    return authRequest;
   }
 
   //getting { currentUser }
@@ -27,17 +33,22 @@ class Auth {
   }
 
   //authenticate user
-  Future<FirebaseUser> loginUser(
+  Future<AuthenticationRequest> loginUser(
   { String emailParam = "", String passwordParam = "" }) async {
+    AuthenticationRequest authRequest =  new AuthenticationRequest();
     try {
-      return await this._auth
+      var user = await this._auth
         .signInWithEmailAndPassword( 
           email: emailParam, password: passwordParam 
-        );   
+        );
+      if( user != null ){
+        authRequest.success = true;
+      }   
     } catch (e) {
       print("[error login user] ::: $e ");
+      this._mapError(authRequest, e.code);
     }
-    return null;
+    return authRequest;
   }
 
   Future<void> logOut() async{
@@ -48,5 +59,27 @@ class Auth {
     }
   }
 
+  /**
+   * Handler Error
+   * in auth
+  */
+  void _mapError( AuthenticationRequest authRequest, String codeReason ){
+    switch(codeReason){
+      case 'ERROR_USER_NOT_FOUND':
+        authRequest.messageError = "Usuario no encontrado";
+      break;
+      case 'ERROR_WRONG_PASSWORD':
+        authRequest.messageError = "Contrasena invalida";
+      break;
+      case 'ERROR_NETWORK_REQUEST_FAILED':
+        authRequest.messageError  = "Error de red";
+      break;
+      case 'ERROR_EMAIL_ALREADY_IN_USER':
+        authRequest.messageError = "Usuario ya registrado";
+      break;
+      default:
+        authRequest.messageError = codeReason;
+    }
+  }
 
 }
