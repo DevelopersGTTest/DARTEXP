6f22a3696f7eb35eb1a5aec5b709fa237ec7b4ee
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-23 11:08:24 -0600

    implements loading

--- messages_app/lib/ui/screens/login_screen.dart
+++ messages_app/lib/ui/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:messages_app/ui/widgets/button_widget.dart';
 import 'package:messages_app/ui/widgets/icon_widget.dart';
 import 'package:messages_app/ui/widgets/text_widget.dart';
 import 'package:messages_app/services/auth_service.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class Login extends StatefulWidget {
   static const String routeName = "/login";
@@ -13,11 +14,21 @@ class _LoginState extends State<Login> {
   //properties 
   String _email = "";
   String _password = "";
+  bool showSpinner = false;
+
+  //controller state
+  void setSpinnerStatus(bool  status){
+    setState(() {
+      this.showSpinner = status;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: ModalProgressHUD(
+        inAsyncCall: this.showSpinner,
+        child: Container(
         padding: EdgeInsets.symmetric(horizontal:  24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -47,15 +58,18 @@ class _LoginState extends State<Login> {
               Colors.blueAccent,
               "Log In",
               () async {
+                this.setSpinnerStatus(true);
                 var loginUser = await Auth().loginUser( 
                   emailParam: this._email, passwordParam:  this._password
                 );
                 if( loginUser != null ){
                   Navigator.pushNamed(context, "/dashboard");
                 }
+                this.setSpinnerStatus(false);
               }
             )
           ],
+        ),
         ),
       ),
     );
