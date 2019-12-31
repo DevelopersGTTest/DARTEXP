ec8e3464a319dfdaec471eed0556c15924712ca1
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-12-30 22:06:53 -0600

    remove dependencies

--- messages_app/lib/ui/widgets/message_widget.dart
+++ messages_app/lib/ui/widgets/message_widget.dart
@@ -29,7 +29,7 @@ class MessageWgt extends StatelessWidget {
               child: Text(
                 this.ctxMessage, 
                 style: TextStyle( 
-                  fontSize: 16.0,  //fixeds styles
+                  fontSize: 16.0,  
                   color: Colors.black54 
                 ), 
               ) 
