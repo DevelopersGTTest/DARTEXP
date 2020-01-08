17e84e1124feb73011fd50ce14f8fb59f29c6a3b
p-jacobo2012240 <pablojacobo1998@gmail.com>
2020-01-07 18:41:50 -0600

    Override methods in searchData

--- search_delegate/lib/dashboard.dart
+++ search_delegate/lib/dashboard.dart
@@ -16,3 +16,29 @@ class Dashboard extends StatelessWidget {
  );
  }
 }
+
+class DataSearch extends SearchDelegate<String>{
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return null;
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    return null;
+  }
+}
